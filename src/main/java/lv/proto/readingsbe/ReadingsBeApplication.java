c0ed6bf96f7d9e40873e2df723541c64cb836ed2
Janis Kalnins <janis.kalnins@accenture.com>
2018-10-16 23:17:08 +0300

    Added login through facebook

--- src/main/java/lv/proto/readingsbe/ReadingsBeApplication.java
+++ src/main/java/lv/proto/readingsbe/ReadingsBeApplication.java
@@ -4,9 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class ReadingsBeApplication {
 
     public static void main(String[] args) {
